f2a1d2869aa3456c98515155310f638e5e15ad09
Randy Dev <90479255+xtsea@users.noreply.github.com>
2024-08-09 15:15:49 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+API_ID = os.environ["API_ID"]
+API_HASH = os.environ["API_HASH"]
+SESSION = os.environ["SESSION"]
+FEDBAN_API_KEY = os.environ["FEDBAN_API_KEY"]
+GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
