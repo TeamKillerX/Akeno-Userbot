76210a77d6e72bf50bb4524991fc7599aa950af0
Randy Dev <90479255+xtsea@users.noreply.github.com>
2024-08-09 15:36:05 +0700

    Create ping.py

---
+++ Akeno/plugins/ping.py
@@ -0,0 +1,47 @@
+import time
+from pyrogram import Client, filters
+from datetime import datetime as dt
+from Akeno import StartTime
+from Akeno.utils.handler import Akeno
+from datetime import datetime as dt
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    readable_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        readable_time += f"{time_list.pop()}, "
+
+    time_list.reverse()
+    readable_time += ":".join(time_list)
+
+    return readable_time
+
+@Akeno(
+    ~filters.scheduled
+    & filters.command(["ping"], ["."])
+    & filters.me
+    & ~filters.forwarded
+)
+async def custom_ping_handler(client: Client, message: Message):
+    uptime = get_readable_time((time.time() - StartTime))
+    start = dt.now()
+    lol = await message.reply_text("**Pong!!**")
+    await asyncio.sleep(1.5)
+    end = dt.now()
+    duration = (end - start).microseconds / 1000
+    await lol.edit_text(
+        f" **Pong !!** " f"`%sms` \n" f" **Uptime** - " f"`{uptime}` " % (duration)
+    )
