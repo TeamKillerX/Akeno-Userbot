e499a77ba696d8a47832fe4e56ac636b3a8c46b5
Randy Dev <90479255+xtsea@users.noreply.github.com>
2024-08-09 14:31:22 +0700

    Create __init__.py

---
+++ Akeno/__init__.py
@@ -0,0 +1,69 @@
+import asyncio
+import logging
+import time
+import pyrogram
+import string
+import random
+from inspect import getfullargspec
+from os import path
+from random import choice
+import aiohttp
+import re
+import os
+
+from datetime import datetime as dt
+from pyrogram import Client
+from pyrogram.types import *
+from pyrogram import filters
+from pyrogram.raw.all import layer
+from pyrogram.handlers import MessageHandler
+from config import API_HASH, API_ID, BOT_TOKEN, SESSION
+from pytgcalls import GroupCallFactory
+from aiohttp import ClientSession
+from Akeno.utils.logger import LOGS
+from platform import python_version
+from pyrogram import __version__ as pyrogram_version
+
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.basicConfig(level=logging.INFO)
+
+if not BOT_TOKEN:
+    print("Warning no bot token")
+
+logger = logging.getLogger(__name__)
+
+StartTime = time.time()
+START_TIME = dt.now()
+CMD_HELP = {}
+clients = []
+ids = []
+act = []
+db = {}
+
+SUDOERS = filters.user()
+    
+aiohttpsession = ClientSession()
+
+app = Client(
+    "userbotme",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="akeno.plugins.bot"),
+    in_memory=True,
+)
+
+client = Client(
+    "one",
+    app_version="latest",
+    device_model="Akeno",
+    system_version="Linux",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string=SESSION,
+    plugins=dict(root="akeno.plugins"),
+)
+if not hasattr(client, "group_call"):
+    setattr(client, "group_call", GroupCallFactory(client).get_group_call())
+
+clients.append(client)
