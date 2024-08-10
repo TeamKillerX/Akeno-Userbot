5cbb952fa471cedf00b407e16e0bcc7c215bb27a
Randy Dev <90479255+xtsea@users.noreply.github.com>
2024-08-10 12:15:19 +0700

    Create limited.py

---
+++ Akeno/plugins/limited.py
@@ -0,0 +1,34 @@
+import asyncio
+import os
+from pyrogram.types import *
+from pyrogram import *
+
+from pyrogram import Client, filters
+from pyrogram import Client as ren
+from pyrogram.types import Message
+from pyrogram.raw import *
+
+from Akeno.utils.handler import Akeno
+from config import CMD_HANDLER
+
+@Akeno(
+    ~filters.scheduled
+    & filters.command(["limit", "limited"], CMD_HANDLER)
+    & filters.me
+    & ~filters.forwarded
+)
+async def spamban(client: Client, message: Message):
+    await client.unblock_user("SpamBot")
+    response = await client.invoke(
+        raw.functions.messages.StartBot(
+            bot=await client.resolve_peer("SpamBot"),
+            peer=await client.resolve_peer("SpamBot"),
+            random_id=client.rnd_id(),
+            start_param="start",
+        )
+    )
+    wait_msg = await message.reply_text("`Processing . . .`")
+    await asyncio.sleep(1)
+    spambot_msg = response.updates[1].message.id + 1
+    status = await client.get_messages(chat_id="SpamBot", message_ids=spambot_msg)
+    await wait_msg.edit_text(f"~ {status.text}")
