b154826c99c708e6776fe67d6c1b8a9b9eb6f038
Randy Dev <90479255+xtsea@users.noreply.github.com>
2024-08-17 12:31:25 +0700

    _

--- Akeno/plugins/antiarabic.py
+++ Akeno/plugins/antiarabic.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2020-2024 (c) Randy W @xtdevs, @xtsea
+#
+# from : https://github.com/TeamKillerX
+# Channel : @RendyProjects
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from pyrogram import Client, filters, idle, enums
 from pyrogram.types import Chat, Message, InlineKeyboardButton, InlineKeyboardMarkup, ChatPrivileges, ChatMember
 from pyrogram.enums.parse_mode import ParseMode
@@ -69,4 +88,5 @@ async def antiarabic_filter(client: Client, message: Message):
             if await can_delete(chat, me.id):
                 return await message.delete()
 
-randydev.run()
+module = modules_help.add_module("antiarabic", __file__)
+module.add_command("askf", "to antarabic auto delete messages")
