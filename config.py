58c8530f172c46d2c5929e74b77a09249972de7e
Randy Dev <90479255+xtsea@users.noreply.github.com>
2024-08-19 00:10:05 +0700

    _

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
 MONGO_URL = os.environ["MONGO_URL"]
 CMD_HANDLER = ["?"]
 cohere_key = os.getenv("COHERE_KEY", "")
-LOG_ID = -1001825872637
+LOG_ID = os.getenv("LOG_ID", None)
 
 AFK_CACHE = {}
 BOT_CMD_INFO = {}
