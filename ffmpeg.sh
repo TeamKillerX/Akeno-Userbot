b7dbb7f95f10834237bcd30f0e17dee555270389
Randy Dev <90479255+xtsea@users.noreply.github.com>
2024-08-20 19:51:11 +0700

    Create ffmpeg.sh

---
+++ ffmpeg.sh
@@ -0,0 +1,6 @@
+wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
+wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz.md5
+md5sum -c ffmpeg-git-amd64-static.tar.xz.md5
+tar xvf ffmpeg-git-amd64-static.tar.xz
+mv ffmpeg-git*/ffmpeg ffmpeg-git*/ffprobe /usr/local/bin/
+echo "Done"
