dd8d169be8d2c1bbfcafc52aa5102635c1270ad0
Randy Dev <90479255+xtsea@users.noreply.github.com>
2024-09-01 02:26:31 +0700

    Create __init__.py

---
+++ Akeno/plugins/__init__.py
@@ -0,0 +1,16 @@
+import glob
+from Akeno.utils.logger import LOGS
+from os.path import basename, dirname, isfile
+
+def __list_all_modules():
+    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+    all_modules = [
+        basename(f)[:-3]
+        for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
+    return all_modules
+
+ALL_MODULES = sorted(__list_all_modules())
+LOGS.info("Modules to load: %s", str(ALL_MODULES))
+__all__ = ALL_MODULES + ["ALL_MODULES"]
